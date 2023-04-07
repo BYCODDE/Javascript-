bf6c554341410473dc9310e7a793cf0737b33c17
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2023-04-07 17:07:49 +0400

    First JS work!
    First JS work!

---
+++ Flags.js
@@ -0,0 +1,189 @@
+
+
+
+
+
+
+
+
+
+
+( function main () {
+  
+  
+  const searchBtnEl = document.getElementsByClassName("bx bx-search");
+    const searchInputEl = document.getElementsByClassName("search-field");
+
+   
+    const root = document.getElementById("mainroot");
+    
+    
+    const API = "https://restcountries.com/v3.1/all"
+    
+    
+    const getCountries = async () => {
+       
+       
+       const response  =  await fetch (API)
+       const allCountires = await  response.json()
+       
+       for (let i= 0; i < allCountires.length; i ++) {
+          const countryContainer = document.createElement("div")
+          const countryFlag = document.createElement("img")
+          countryFlag.src = allCountires[i].flags.png
+          
+          countryContainer.appendChild(countryFlag)
+          
+          
+          
+ 
+ 
+ 
+    const countryName = document.createElement("h2")
+    countryName.innerText = allCountires[i].name.common
+    countryContainer.appendChild(countryName)
+    
+ 
+ 
+ 
+ 
+    countryCapital = document.createElement("p")
+    countryCapital.innerText =  "Capital:" + " " + allCountires[i].capital 
+    countryContainer.appendChild(countryCapital)
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    countryPopulation = document.createElement("p")
+    countryPopulation.innerText =  "Population: " + allCountires[i].population 
+    countryContainer.appendChild(countryPopulation)
+    
+ 
+ 
+ 
+    countryRegion = document.createElement("p")
+    countryRegion.innerText = "Region: " + allCountires[i].region
+    countryContainer.appendChild(countryRegion)
+ 
+    root.append(countryContainer)
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+  
+  
+};
+   
+ 
+ 
+ getCountries()
+ 
+ })();
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
+      let getMode = localStorage.getItem("mode");
+          if(getMode && getMode === "dark-mode"){
+            body.classList.add("dark");
+          }
+
+// js code to toggle dark and light mode
+      modeToggle.addEventListener("click" , () =>{
+        modeToggle.classList.toggle("active");
+        body.classList.toggle("dark");
+
+        // js code to keep user selected mode even page refresh or file reopen
+        if(!body.classList.contains("dark")){
+            localStorage.setItem("mode" , "light-mode");
+        }else{
+            localStorage.setItem("mode" , "dark-mode");
+        }
+      });
+
+// js code to toggle search box
+        searchToggle.addEventListener("click" , () =>{
+        searchToggle.classList.toggle("active");
+      });
+ 
+      
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click" , () =>{
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e =>{
+    let clickedElm = e.target;
+
+    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
